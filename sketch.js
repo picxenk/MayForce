6e250e782bb907e40b9d45a0d4aecef504e43779
SeungBum Kim <picxenk@gmail.com>
2025-11-04 11:28:52 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
+let ball;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
+
+  ball = new Particle();
 }
+
 
 function draw() {
   background(220);
-  circle(100, 100, 100);
+
+  let gravity = createVector(0, 0.3);
+  ball.addForce(gravity);
+
+  ball.update();
+  ball.show();
 }
